6c8b70fd2a9360155154dca3f5f8038b1332d264
Matthew Pacey <paceym@oregonstate.edu>
2023-05-27 10:31:21 -0700

    Completed assignment 8

---
+++ hw8-docker/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import boats
+import loads
+
+"""
+CS 493 - HW4 - Marina Intermediate REST API
+Matthew Pacey
+"""
+
+app = Flask(__name__)
+app.register_blueprint(boats.bp)
+app.register_blueprint(loads.bp)
+
+
+@app.route('/')
+def index():
+    return "Please navigate to /boats or /loads to use this API"
+
+
+if __name__ == '__main__':
+    # app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
